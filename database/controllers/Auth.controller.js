3603033aed07d6fb0505c3ca350f8fbca7d68d25
FalejevV <v.o.v.a@hotmail.com>
2022-11-07 10:17:44 +0200

    domain change

--- database/controllers/Auth.controller.js
+++ database/controllers/Auth.controller.js
@@ -27,7 +27,7 @@ class AuthController{
                     secure: true,
                     httpOnly: true,
                     sameSite: 'none',
-                    domain: 'http://localhost',
+                    domain: 'https://iaeround.xyz',
                   });
                 res.send({ 
                     status:"OK",
