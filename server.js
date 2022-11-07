9b87aa28fb07bfb11d9baaf4771294fb009e9717
FalejevV <v.o.v.a@hotmail.com>
2022-11-07 22:02:11 +0200

    origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin: "https://iaeround.xyz/", 
+  origin: "https://iaeround.xyz", 
   methods: ['GET', 'PUT', 'POST'], 
   allowedHeaders: ['Content-Type', 'Authorization', '*'], 
   credentials: true, 
