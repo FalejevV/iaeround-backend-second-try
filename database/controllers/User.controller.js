faa9a7c8b98220d9c3c51f4701be4b86d099d47c
FalejevV <v.o.v.a@hotmail.com>
2022-11-27 21:11:27 +0200

    storage controller destination change

--- database/controllers/User.controller.js
+++ database/controllers/User.controller.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { symbolCheck, bodyInjectionCheck } = require('../VarChecker');
 const StorageController = require("../../storage/StorageController");
+
 const xid = require('xid-js');
 
 class UserController{
